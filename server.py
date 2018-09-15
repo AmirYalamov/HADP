5e217a51ee639c6b5166b935fb69695cd3ff722a
Aman Adhav <amanadhav@gmail.com>
2018-09-15 08:47:02 -0400

    adding new cloud service files

--- server.py
+++ server.py
@@ -17,12 +17,22 @@ class Analysis :
 app = Flask(__name__)
 @app.route('/api/message', methods=['POST'])
 def message_recieve():
-    message_recieved = request.get_json(force=True)
+    message_recieved = request.get_json()
+    print(message_recieved["message"])
     required = ['message','image','video'] #defines what are our 3 different platforms
-    if not all(k in values for k in required):
-        return "Missing values", 400
-    
+    '''
+    if not all(k in message_recieved for k in required):
+        return "Missing values", 400    
+    '''
     response = {'sentiment analysis' : (Analysis.analyzed_sentence),
-                'place interest' : (Analysis.analyzed_place_interest)
+                'place interest' : (Analysis.analyzed_place_interest),
                 }
     return jsonify(response), 200
+
+if __name__ == '__main__':
+    from argparse import ArgumentParser
+    parser = ArgumentParser()
+    parser.add_argument('-p', '--port', default=5000, type=int,help='port to listen on')
+    args = parser.parse_args()
+    port = args.port
+    app.run(host="0.0.0.0", port = port)
