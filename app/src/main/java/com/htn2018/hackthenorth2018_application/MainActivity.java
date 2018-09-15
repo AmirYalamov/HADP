c88febe1f74e4193e88bced1ee72939ad60bb2c1
Rohan Gupta <rohangupta.0927@gmail.com>
2018-09-15 01:19:36 -0400

    Initial commit of all build files for Android application

---
+++ app/src/main/java/com/htn2018/hackthenorth2018_application/MainActivity.java
@@ -0,0 +1,13 @@
+package com.htn2018.hackthenorth2018_application;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
