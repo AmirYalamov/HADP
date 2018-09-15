f0d97f22a66423c62482e408add500f203630cb1
Rohan Gupta <rohangupta.0927@gmail.com>
2018-09-15 09:50:19 -0400

    added custom keyboard

--- app/src/main/java/com/htn2018/hackthenorth2018_application/MainActivity.java
+++ app/src/main/java/com/htn2018/hackthenorth2018_application/MainActivity.java
@@ -2,6 +2,8 @@ package com.htn2018.hackthenorth2018_application;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
 
